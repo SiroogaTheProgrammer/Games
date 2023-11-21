2fed300390e273252051085edb4bb09ab1421635
SiroogaTheProgrammer <148899581+SiroogaTheProgrammer@users.noreply.github.com>
2023-11-21 21:40:54 +0200

    Create TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,9 @@
+print("TIC TAC TOE")
+
+gameField = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+
+def printGameField():
+    for i in range(0, 3):
+        print(gameField[i])
+
+inGame = True
